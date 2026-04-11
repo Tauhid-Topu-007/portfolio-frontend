abe22e1d77f3aede6b26baced22b8f0ae4d04b0b
tauhid-topu <t.topu021@gmail.com>
2026-04-12 01:05:27 +0600

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './context/AuthContext';
@@ -23,6 +23,39 @@ import ContactPage from './pages/ContactPage';
 import AdminLoginPage from './pages/AdminLoginPage';
 import AdminPage from './pages/AdminPage';
 
+// Wrapper component to use useLocation hook
+function AppContent() {
+  const location = useLocation();
+  const isAdminRoute = location.pathname === '/admin' || location.pathname.startsWith('/admin/');
+  
+  return (
+    <>
+      <ScrollToTop />
+      <CustomCursor />
+      <ParticleBackground />
+      <div className="relative z-10">
+        {/* Only show Navbar on non-admin routes */}
+        {!isAdminRoute && <Navbar />}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:id" element={<ProjectDetailPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogDetailPage />} />
+          <Route path="/research" element={<ResearchPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/admin/login" element={<AdminLoginPage />} />
+          <Route path="/admin/*" element={<AdminPage />} />
+        </Routes>
+        {/* Only show Footer on non-admin routes */}
+        {!isAdminRoute && <Footer />}
+      </div>
+      <Toaster position="top-right" />
+    </>
+  );
+}
+
 function App() {
   return (
     <HelmetProvider>
@@ -30,26 +63,7 @@ function App() {
         <AuthProvider>
           <DataProvider>
             <Router>
-              <ScrollToTop />
-              <CustomCursor />
-              <ParticleBackground />
-              <div className="relative z-10">
-                <Navbar />
-                <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/about" element={<AboutPage />} />
-                  <Route path="/projects" element={<ProjectsPage />} />
-                  <Route path="/projects/:id" element={<ProjectDetailPage />} />
-                  <Route path="/blog" element={<BlogPage />} />
-                  <Route path="/blog/:slug" element={<BlogDetailPage />} />
-                  <Route path="/research" element={<ResearchPage />} />
-                  <Route path="/contact" element={<ContactPage />} />
-                  <Route path="/admin/login" element={<AdminLoginPage />} />
-                  <Route path="/admin/*" element={<AdminPage />} />
-                </Routes>
-                <Footer />
-              </div>
-              <Toaster position="top-right" />
+              <AppContent />
             </Router>
           </DataProvider>
         </AuthProvider>
