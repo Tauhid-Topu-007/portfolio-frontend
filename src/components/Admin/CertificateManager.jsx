47b1305222c5a9b092836acac38b685127495427
tauhid-topu <t.topu021@gmail.com>
2026-04-12 22:11:07 +0600

    admin catch

--- src/components/Admin/CertificateManager.jsx
+++ src/components/Admin/CertificateManager.jsx
@@ -201,7 +201,7 @@ const CertificateManager = () => {
         </div>
       )}
 
-      {/* Modal - Keep same as before */}
+      {/* Modal */}
       {showModal && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
           <div className="bg-white dark:bg-gray-800 rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto">
